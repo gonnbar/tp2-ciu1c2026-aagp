f2d01ce2a7c6953ac915fa6dc1acc2c5422b568f
ailenperalta <ailen.peralta@estudiantes.unahur.edu.ar>
2026-06-26 18:04:27 -0300

    Agrego estructura de carpetas y paleta de colores

---
+++ src/styles/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    primary: "#6D42D8",
+    primaryDark: "#5631B8",
+    secondary: "#B99AF5",
+
+    background: "#F7F7FA",
+    surface: "#FFFFFF",
+
+    text: "#242424",
+    textSecondary: "#757575",
+
+    border: "#E5E5EA",
+
+    success: "#51B37A",
+    error: "#E55A5A",
+
+    warning: "#F5B942"
+};
