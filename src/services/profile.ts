068fb02867f98db1d4469b1aba6649f47a931a5d
pazm-avila <pazmaria.avila@estudiantes.unahur.edu.ar>
2026-06-29 20:57:49 -0300

    Fix Profile service + rutas descomentadas App

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,5 +1,4 @@
 import { API_URL } from "./config";
-import type { Post } from "../types/Post";
 
 export async function getUserPosts(id: string) {
   const response = await fetch(`${API_URL}/users/${id}/posts`);
@@ -8,8 +7,7 @@ export async function getUserPosts(id: string) {
     throw new Error("Error al obtener posts.");
   }
 
-  const userPosts: Post[] = await response.json();
+  const data = await response.json();
 
-  return userPosts;
-  
+  return data.posts;
 }
