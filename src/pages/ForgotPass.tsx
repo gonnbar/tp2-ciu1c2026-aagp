ce32fea718e29ffc6f8d5ec07a036e4437969047
gonnbar <gonzalonicolasbarbosa@gmail.com>
2026-06-30 10:45:39 -0300

    add forgot pass page

---
+++ src/pages/ForgotPass.tsx
@@ -0,0 +1,193 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import type { User } from "../types/User"
+import logo from "../assets/imageLOGO.png"
+
+export default function ForgotPassword() {
+  const navigate = useNavigate();
+
+  const [step, setStep] = useState(1);
+
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [user, setUser] = useState<User | null>(null);
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  async function handleContinue(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    setError("");
+
+    try {
+      // Buscar usuario
+      const usersRes = await fetch("http://localhost:3000/users");
+      const users = await usersRes.json();
+
+      const foundUser = users.find(
+        (u: any) => u.nickname === nickname
+      );
+
+      if (!foundUser) {
+        setError("Usuario inexistente.");
+        return;
+      }
+
+      // Obtener datos completos
+      const userRes = await fetch(
+        `http://localhost:3000/users/${foundUser._id}`
+      );
+
+      const data = await userRes.json();
+
+      if (data.user.email !== email) {
+        setError("El email no coincide con el usuario.");
+        return;
+      }
+
+      setUser(data.user);
+      setStep(2);
+
+    } catch {
+      setError("Ocurrió un error.");
+    }
+  }
+
+  async function handleChangePassword(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    setError("");
+
+    if (newPassword !== confirmPassword) {
+      setError("Las contraseñas no coinciden.");
+      return;
+    }
+
+    if (!user) return;
+
+    try {
+      const updatedUser = {
+        nickname: user.nickname,
+        email: user.email,
+        password: newPassword,
+      };
+      const response = await fetch(
+        `http://localhost:3000/users/${user._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedUser),
+        }
+      );
+
+      const body = await response.text();
+      console.log(body);
+      if (!response.ok) {
+        setError("No se pudo actualizar la contraseña.");
+        return;
+      }
+
+      setSuccess("Contraseña actualizada correctamente.");
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+
+    } catch {
+      setError("Ocurrió un error.");
+    }
+  }
+
+  return (
+    <div className="flex min-h-ful flex-col justify-center px-6 py-12 lg:px-8">
+
+      <div className="rounded-2xl bg-white p-6 shadow-md flex flex-col justify-center px-4 py-6 lg:px-5 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+
+          <img src={logo} alt="Logo UnaHur" className="mx-auto h-15 w-auto" />
+          <h2 className="mt-4 text-center text-2xl font-bold tracking-tight text-text">
+            Recuperar contraseña
+          </h2>
+
+          {step === 1 && (
+            <form
+              onSubmit={handleContinue}
+              className="p-6 flex flex-col gap-3"
+            >
+              <input
+                className="w-full border border-gray-300 rounded-lg p-2"
+                type="text"
+                placeholder="Nickname"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+
+              <input
+                className="w-full border border-gray-300 rounded-lg p-2"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <button
+                className="rounded-lg bg-violet-600 px-4 py-2 text-white hover:bg-violet-700"
+              >
+                Continuar
+              </button>
+            </form>
+          )}
+
+          {step === 2 && (
+            <form
+              onSubmit={handleChangePassword}
+              className="p-6 flex flex-col gap-3"
+            >
+              <input
+                className="w-full border border-gray-300 rounded-lg p-2"
+                type="password"
+                placeholder="Nueva contraseña"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+
+              <input
+                className="w-full border border-gray-300 rounded-lg p-2"
+                type="password"
+                placeholder="Confirmar contraseña"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+
+              <button
+                className="rounded-lg bg-violet-600 px-4 py-2 text-white hover:bg-violet-700"
+              >
+                Cambiar contraseña
+              </button>
+            </form>
+          )}
+
+          {error && (
+            <p className="mt-4 text-center text-sm text-red-500">
+              {error}
+            </p>
+          )}
+
+          {success && (
+            <p className="mt-4 text-center text-sm text-green-600">
+              {success}
+            </p>
+          )}
+
+        </div>
+      </div>
+    </div>
+  );
+}
